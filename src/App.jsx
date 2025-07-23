c21b9d3ecccd1938abcfa6f34ab3d8e227c41344
gnaneswararaoippili <gnaneswararaoippili@gmail.com>
2025-07-23 09:36:58 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router>
+    <Router basename="/CICD">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
